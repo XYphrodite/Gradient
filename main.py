597022874ffe378ef6db3317b3506c34bd8e93ec
XYphrodite <72796194+XYphrodite@users.noreply.github.com>
2022-11-25 09:32:36 +0300

    2g

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from math import *
 import numpy as np
 import matplotlib.pyplot as plt
+import threading
 
+# threading._DummyThread._Thread__stop = lambda x: 42
 E = 0.01
 dlt = E / 100
 
@@ -24,7 +26,7 @@ def foo(x1, x2, n=0):
     if n == 0:
         return fooA(x1, x2)
     else:
-        return fooB(x[0], x[1])
+        return fooB(x1, x2)
 
 
 # вектор градиента
@@ -41,12 +43,13 @@ def grad(x, n):
 
 def gradient(e, x, n):
     Iterations = 0
-    h = 0.01
+    h = 0.00005
     gradient_vector = grad(x, n)
     while (pow(gradient_vector[0] + gradient_vector[1], 2) > e):
         x = [x[0] - h * gradient_vector[0], x[1] - h * gradient_vector[1]]
         gradient_vector = grad(x, n)
         Iterations += 1
+        # print(Iterations)
     return x, Iterations
 
 
@@ -93,10 +96,10 @@ def down(e, x, n):
 
 
 x = [10, 10]
-xG, N1 = gradient(dlt/5500, x, 0)
+xG, N1 = gradient(dlt / 5, x, 0)
 xD, N2 = down(dlt / 5, x, 0)
-xG1, N11 = gradient(dlt / 42, x, 1)
-xD1, N21 = down(dlt / 42, x, 1)
+xG1, N11 = gradient(dlt, x, 1)
+# xD1, N21 = down(dlt, x, 1)
 print("Градиент: ", xG)
 print("Количество шагов: ", N1)
 print("Метод наискорейшего спуска: ", xD)
@@ -104,10 +107,10 @@ print("Количество шагов: ", N2)
 
 print("Gradient solution: ", xG1)
 print("Iterations: ", N11)
-print("Steepest gradient solution: ", xD1)
-print("Iterations: ", N21)
+# print("Steepest gradient solution: ", xD1)
+# print("Iterations: ", N21)
 
-fig, (ax2) = plt.subplots(1, 1)
+fig, (ax2, ax1) = plt.subplots(1, 2)
 
 levels = [-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.10, -0.05, -0.01, 0.0, 0.01, 0.05, 0.10, 0.20, 0.30,
           0.4,
@@ -120,14 +123,15 @@ ya = f2(xa1, xa2)
 conta = ax2.contour(xa1, xa2, ya, levels=levels)
 ax2.plot(xG[0], xG[1], color="red", marker=".")
 ax2.plot(xD[0], xD[1], color="blue", marker=".")
-"""
-xb1 = np.arange(-27, 28, 0.125)
-xb2 = np.arange(-24, 26, 0.125)
+
+levels = [0, 0.5,0.75, 1, 1.5]
+xb1 = np.arange(-1, 3, 0.01)
+xb2 = np.arange(-1, 3, 0.01)
 xb1, xb2 = np.meshgrid(xb1, xb2)
 f2b = np.vectorize(fooB)
 yb = f2b(xb1, xb2)
-contb = ax1.contour(xb1, xb2, yb, levels=10)
+contb = ax1.contour(xb1, xb2, yb, levels=levels)
 ax1.plot(xG1[0], xG1[1], color="pink", marker=".")
-ax1.plot(xD1[0], xD1[1], color="blue", marker=".")
-"""
+# ax1.plot(xD1[0], xD1[1], color="blue", marker=".")
+
 plt.show()
